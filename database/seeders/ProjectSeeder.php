1e3768b4082536bed736b9dfbcf2d37d916f0d17
Attila Zsolt Danku <attilazsoltdanku@attilas-mbp.t.hu>
2022-03-27 20:32:40 +0200

    generated model -m -r -s -f, policy, requests for projects

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,20 +2,21 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ProjectSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(UserSeeder::class);
-        $this->call(ProjectSeeder::class);
+        Project::create([
+
+        ]);
     }
 }
